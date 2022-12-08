22c7f490ed142ab7a73e3681d5880ace2513c41c
dedojs <xandresousax@gmail.com>
2022-12-08 10:31:02 -0300

    feat: controller/model filme

---
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,24 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine($"Filme {filme.Titulo} adicionado com sucesso!");
+    }
+
+    [HttpGet]
+    public List<Filme> BuscarFilmes()
+    {
+        return filmes;
+    }
+}
---
+++ FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "Título obrigatório")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "Gênero Obrigatório")]
+    [MaxLength(50, ErrorMessage = "Não pode exceder 50 caracteres")]
+    public string Genero { get; set; }
+    [Required]
+    [Range(70, 400, ErrorMessage = "A duração deve ter entre 70 e 400 minutos")]
+    public int Duracao { get; set;}
+
+}
